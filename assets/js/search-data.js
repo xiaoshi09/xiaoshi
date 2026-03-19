ab5fdc591525d0bf96efa624cda8f7910f6f3266
xiaoshi09 <77429371+xiaoshi09@users.noreply.github.com>
2026-03-19 01:00:11 +0000

    Deploying to gh-pages from @ xiaoshi09/xiaoshi@b9b2563f3e7e34c0773fce6c68ae2810d6a89de8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -468,9 +468,9 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/xiaoshi/projects/9_project/";
-            },},{id: "teachings-data-science-fundamentals",
-          title: 'Data Science Fundamentals',
-          description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
+            },},{id: "teachings-usp4-introduction-to-geographic-information-systems-gis",
+          title: 'USP4. Introduction to Geographic Information Systems (GIS)',
+          description: "This is a hands-on introduction to practical knowledge of Geographical Information Systems and Science designed for students in urban planning and related fields. Maps are a powerful tool for both describing and studying urban issues that occur across space. Lectures and labs will discuss and focus on the elements of effectively creating, analyzing, and presenting maps in urban settings. By the end of the course, students can expect to be able to find and manage spatial data, run spatial analyses, and present output in maps and data files.",
           section: "Teachings",handler: () => {
               window.location.href = "/xiaoshi/teachings/data-science-fundamentals/";
             },},{id: "teachings-introduction-to-machine-learning",
